ba234babf4b7a9e6f9763dc957cb2feb0b7023e2
fsecuritynz <45273776+fsecuritynz@users.noreply.github.com>
2025-05-19 11:35:46 +1200

    Update netbox_firewall_address.tf

--- netbox_firewall_address.tf
+++ netbox_firewall_address.tf
@@ -1,13 +1,1 @@
-# Resource block creating firewall addresses per vdom
-resource "fortios_firewall_address" "netbox_objects" {
-  for_each = local.address_objects
 
-  name      = each.value.name
-  subnet    = each.value.subnet
-  #comment   = each.value.comment
-  type      = "ipmask"
-  color     = each.value.color
-
-  # Use ternary to assign vdomparam or empty string if null
-  vdomparam = each.value.vdom != null ? each.value.vdom : ""
-}
